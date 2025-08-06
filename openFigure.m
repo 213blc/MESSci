e1030e2449bf053cf7462c34e748ccf713b6363f
213blc <Benjamin.Hatchett@gmail.com>
2025-08-05 21:58:04 -0700

    Update openFigure.m

--- openFigure.m
+++ openFigure.m
@@ -1,6 +1,6 @@
 function openFigure(width,height,units)
 %%
-% openFigure.m pops open a figure with the specified dimensions and units. Note these
+% openFigure.m opens a figure with the specified dimensions and units. Note these
 % dimensions do not appear to be preserved during export in all cases.
 % Example: openFigure(35,23,'centimeters');
 %%
